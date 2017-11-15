d5951c5e4ed833405522d4ebad2c378dcc320424
momo919 <13682018723@163.com>
2017-11-15 14:31:13 +0800

    部分mongodb 操作接口

--- mall-auth/src/main/java/org/jiumao/mall/OAuth2/OAuth2Provider.java
+++ mall-auth/src/main/java/org/jiumao/mall/OAuth2/OAuth2Provider.java
@@ -5,35 +5,47 @@ import org.jiumao.mall.OAuth2.code.ReturnCode;
 import org.jiumao.mall.OAuth2.code.CodeToken;
 import org.jiumao.mall.OAuth2.implicit.ImplicitToken;
 import org.jiumao.mall.OAuth2.password.PasswdToken;
+import org.jiumao.wechatMall.dao.OauthAccessTokenDao;
+import org.jiumao.wechatMall.dao.OauthClientDetailsDao;
+import org.jiumao.wechatMall.dao.OauthCodeDao;
 
 public class OAuth2Provider {
 
+    private OauthCodeDao          codeDao;
+    private OauthAccessTokenDao   accessTokenDao;
+    private OauthClientDetailsDao clientDetailsDao;
     public static ReturnCode giveCode(String clientId) {
+
         return null;
     }
 
     public static CodeToken hasCode(String code) {
         // TODO Auto-generated method stub
+
         return null;
     }
 
     public static ImplicitToken giveToken(String client_id) {
         // TODO Auto-generated method stub
+
         return null;
     }
 
     public static PasswdToken login(String username, String password) {
         // TODO Auto-generated method stub
+
         return null;
     }
 
     public static PasswdToken refresh(String client_id, String client_secret, String refresh_token) {
         // TODO Auto-generated method stub
+
         return null;
     }
 
     public static ClientToken client(String client_id, String client_secret) {
         // TODO Auto-generated method stub
+
         return null;
     }
 
